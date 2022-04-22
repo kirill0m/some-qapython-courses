afae0de8c93a88cde1d6243e80da2c3515f1d688
kirillom <kirillomirea@gmail.com>
2022-04-22 15:01:25 +0300

    replaced string marks

--- homework3/conftest.py
+++ homework3/conftest.py
@@ -4,7 +4,7 @@ from client import ApiClient
 
 @pytest.fixture(scope='session')
 def credentials():
-    with open('homework3\\creds.txt', 'r') as f:
+    with open("homework3\\creds.txt", 'r') as f:
         email = f.readline().strip()
         password = f.readline().strip()
     return email, password
