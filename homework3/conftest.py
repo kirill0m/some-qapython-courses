bb70817659de074b8cfd8933913b1d63a3a9393c
kirillom <kirillomirea@gmail.com>
2022-04-22 14:47:26 +0300

    removed prefix r from path to creds

--- homework3/conftest.py
+++ homework3/conftest.py
@@ -4,7 +4,7 @@ from client import ApiClient
 
 @pytest.fixture(scope='session')
 def credentials():
-    with open(r'homework3\creds.txt', 'r') as f:
+    with open('homework3\\creds.txt', 'r') as f:
         email = f.readline().strip()
         password = f.readline().strip()
     return email, password
